2d9e1f4940de53c8e148ce8127fc9d096a4bdbef
pawelbak300589 <pawelbak300589@gmail.com>
2020-03-05 19:24:05 +0000

    Add gear controller and model

---
+++ app/Gear.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Gear extends Model
+{
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'description'
+    ];
+
+    /**
+     * The attributes excluded from the model's JSON form.
+     *
+     * @var array
+     */
+    protected $hidden = [];
+}
---
+++ app/Http/Controllers/GearController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Gear;
+use Illuminate\Http\Request;
+
+class GearController extends Controller
+{
+
+    public function showAllGears()
+    {
+        return response()->json(Gear::all());
+    }
+
+    public function showOneGear($id)
+    {
+        return response()->json(Gear::find($id));
+    }
+
+    public function create(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|max:255',
+        ]);
+
+        $author = Gear::create($request->all());
+
+        return response()->json($author, 201);
+    }
+
+    public function update($id, Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|max:255',
+        ]);
+
+        $author = Gear::findOrFail($id);
+        $author->update($request->all());
+
+        return response()->json($author, 200);
+    }
+
+    public function delete($id)
+    {
+        Gear::findOrFail($id)->delete();
+        return response('Deleted Successfully', 200);
+    }
+}

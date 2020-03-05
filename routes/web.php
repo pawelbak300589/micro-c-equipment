3d2133dbac293e415d866c15e091f2f552123416
pawelbak300589 <pawelbak300589@gmail.com>
2020-03-05 20:04:42 +0000

    Update web routes

--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,8 @@ $router->get('/', function () use ($router)
 {
     return $router->app->version();
 });
-$router->group(['middleware' => 'auth'], function () use ($router)
-{
+//$router->group(['middleware' => 'auth'], function () use ($router)
+//{
     $router->group(['prefix' => 'api'], function () use ($router)
     {
         $router->get('brands', ['uses' => 'BrandController@showAllBrands']);
@@ -24,5 +24,17 @@ $router->group(['middleware' => 'auth'], function () use ($router)
         $router->post('brands', ['uses' => 'BrandController@create']);
         $router->delete('brands/{id}', ['uses' => 'BrandController@delete']);
         $router->put('brands/{id}', ['uses' => 'BrandController@update']);
+
+        $router->get('gears', ['uses' => 'GearController@showAllGears']);
+        $router->get('gears/{id}', ['uses' => 'GearController@showOneGear']);
+        $router->post('gears', ['uses' => 'GearController@create']);
+        $router->delete('gears/{id}', ['uses' => 'GearController@delete']);
+        $router->put('gears/{id}', ['uses' => 'GearController@update']);
+
+        $router->get('categories', ['uses' => 'CategoryController@showAllCategories']);
+        $router->get('categories/{id}', ['uses' => 'CategoryController@showOneCategory']);
+        $router->post('categories', ['uses' => 'CategoryController@create']);
+        $router->delete('categories/{id}', ['uses' => 'CategoryController@delete']);
+        $router->put('categories/{id}', ['uses' => 'CategoryController@update']);
     });
-});
+//});

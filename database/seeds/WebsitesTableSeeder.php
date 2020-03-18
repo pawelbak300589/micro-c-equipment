98c6a3323e35b880c8e87782a5af2fb4f1425b38
Pawel Bak <PawelBak@staveleyhead.co.uk>
2020-03-18 10:48:49 +0000

    Add new websites to the table seeder

--- database/seeds/WebsitesTableSeeder.php
+++ database/seeds/WebsitesTableSeeder.php
@@ -18,6 +18,13 @@ class WebsitesTableSeeder extends Seeder
             ['name' => 'RockRun', 'url' => 'https://rockrun.com/', 'created_at' => date('Y-m-d H:i:s'), 'updated_at' => date('Y-m-d H:i:s')],
             ['name' => 'TrekkInn', 'url' => 'https://www.trekkinn.com/', 'created_at' => date('Y-m-d H:i:s'), 'updated_at' => date('Y-m-d H:i:s')],
             ['name' => 'WeighMyRack', 'url' => 'https://blog.weighmyrack.com/', 'created_at' => date('Y-m-d H:i:s'), 'updated_at' => date('Y-m-d H:i:s')],
+            ['name' => 'Decathlon', 'url' => 'https://www.decathlon.co.uk/', 'created_at' => date('Y-m-d H:i:s'), 'updated_at' => date('Y-m-d H:i:s')],
+            ['name' => 'CotswoldOutdoor', 'url' => 'https://www.cotswoldoutdoor.com/', 'created_at' => date('Y-m-d H:i:s'), 'updated_at' => date('Y-m-d H:i:s')],
+            ['name' => 'EllisBrigham', 'url' => 'https://www.ellis-brigham.com/', 'created_at' => date('Y-m-d H:i:s'), 'updated_at' => date('Y-m-d H:i:s')],
+            ['name' => 'GoOutdoors', 'url' => 'https://www.gooutdoors.co.uk/', 'created_at' => date('Y-m-d H:i:s'), 'updated_at' => date('Y-m-d H:i:s')],
+            ['name' => 'BananaFingers', 'url' => 'https://www.bananafingers.co.uk/', 'created_at' => date('Y-m-d H:i:s'), 'updated_at' => date('Y-m-d H:i:s')],
+            ['name' => 'TheClimbingDepot', 'url' => 'https://www.theclimbingdepot.co.uk/shop', 'created_at' => date('Y-m-d H:i:s'), 'updated_at' => date('Y-m-d H:i:s')],
+            ['name' => 'DicksClimbing', 'url' => 'https://www.dicksclimbing.com/', 'created_at' => date('Y-m-d H:i:s'), 'updated_at' => date('Y-m-d H:i:s')],
         ];
         DB::table('websites')->insert($websites);
     }
